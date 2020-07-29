3e2245baae6ec7c84eda25f15dfb637e0101f445
wuzihan <823429236@qq.com>
2020-07-29 21:03:35 +0800

    test

--- exam-teacher-master/src/containers/student_master/q_checkin/index.js
+++ exam-teacher-master/src/containers/student_master/q_checkin/index.js
@@ -250,6 +250,21 @@ class QCheckin extends React.Component {
 
   render() {
 
+  //   {
+  //     "userId":4,
+  //     "examId":7,
+  //     "answerList":[
+  //         {
+  //             "questionId":1,
+  //             "answer":"A"
+  //         },
+  //         {
+  //             "questionId":2,
+  //             "answer":"A"
+  //         }
+  //     ]
+  // }
+
     const { getFieldDecorator } = this.props.form;
     const { setFieldsValue } = this.props.form;
 
