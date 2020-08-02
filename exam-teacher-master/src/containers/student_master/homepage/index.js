0d4f012d578a4461e61ad363c69818ec46b72c81
git-user-liu <974134113@qq.com>
2020-08-02 14:50:02 +0800

    登录

--- exam-teacher-master/src/containers/student_master/homepage/index.js
+++ exam-teacher-master/src/containers/student_master/homepage/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Icon } from 'antd';
+import { Card, Icon,Carousel,Calendar} from 'antd';
 import { Link } from 'react-router-dom';
 
 export default class Homepage extends React.Component {
@@ -10,21 +10,29 @@ export default class Homepage extends React.Component {
         }
     }
 
-    render() {
+    render() {    
         return (
-            <div className="exam-notice">
-                <el-card className="box-card">
-                    <div slot="header" className="clearfix">
-                        <h2>考试须知</h2>
+            <div>
+                <Carousel autoplay>    
+                    <div className="exam-notice">
+                        <el-card className="box-card"> 
+                            <div slot="header" className="clearfix">
+                                <h2>考试须知</h2>
+                            </div>
+                            <div className="text">    
+                                <p>一、考试为在线考试，有时间限制</p>
+                                <p>二、考试期间不得离开考试页面，否则离开两次将自动交卷</p>
+                                <p>三、考试请独立思考，不得与他人交流</p>
+                                <p>四、若还没开考，请耐心等待</p>
+                            </div>
+                        </el-card>
                     </div>
-                    <div className="text">
-                        <p>一、考试为在线考试，有时间限制</p>
-                        <p>二、考试期间不得离开考试页面，否则离开两次将自动交卷</p>
-                        <p>三、考试请独立思考，不得与他人交流</p>
-                        <p>四、若还没开考，请耐心等待</p>
-                    </div>
-                </el-card>
-            </div>
+                    <div>
+                        <span><h1><a href="#/student_master/personal_center/change_password">个人中心</a></h1></span>
+                    </div>    
+                </Carousel>
+            
+        </div>
         )
     }
 
